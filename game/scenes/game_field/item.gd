db36113e5d4f4809e2a8b9eb1ce59909635eb6d1
Sergey Nikitin <medex81@vk.com>
2024-09-13 22:52:57 +0300

    Удаление неиспользуемых файлов и ресурсов.

--- game/scenes/game_field/item.gd
+++
@@ -1,7 +0,0 @@
-extends Sprite2D
-
-class_name Item
-
-@export var is_blocked: bool = false
-@export var item_type:Match3Logic.EItemTypes = Match3Logic.EItemTypes.NONE
-@export var sound_bouns: AudioStream
