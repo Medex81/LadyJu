fe8838558717b07f92d69002fb9813afaf4e2fd8
Sergey Nikitin <medex81@vk.com>
2024-09-25 16:53:00 +0300

    Добавлен компонент сдвига объекта.

---
+++ game/components/animations/move.gd
@@ -0,0 +1,12 @@
+extends Node
+
+@export var _move_time:float = 1
+
+func task(item:Object, position_to:Vector2, finish_callback:Callable):
+	if item != null and "position" in item and finish_callback.is_valid():
+		var tween_move = create_tween()
+		tween_move.tween_property(item, "position", position_to,  _move_time)
+		tween_move.tween_callback(finish_callback)
+	else:
+		print("Error. Component{0}, object type to move {1}, object has property -position- {2}, calback is valid {3}"\
+		.format([name, item.get_class() if item else null, "position" in item, finish_callback.is_valid()]))
