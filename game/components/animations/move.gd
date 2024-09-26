1fac88d1d7353ab14872b64539bbe3b9db6aa16d
Sergey Nikitin <medex81@vk.com>
2024-09-26 13:29:34 +0300

    Исправлена ошибка отсутствия вызова калбека при неудачном запуске анимации перемещения.

--- game/components/animations/move.gd
+++ game/components/animations/move.gd
@@ -1,6 +1,6 @@
 extends Node
 
-@export var _move_time:float = 1
+@export var _move_time:float = 0.3
 
 func task(item:Object, position_to:Vector2, finish_callback:Callable):
 	if item != null and "position" in item and finish_callback.is_valid():
@@ -8,5 +8,7 @@ func task(item:Object, position_to:Vector2, finish_callback:Callable):
 		tween_move.tween_property(item, "position", position_to,  _move_time)
 		tween_move.tween_callback(finish_callback)
 	else:
+		if finish_callback.is_valid():
+			finish_callback.call()
 		print("Error. Component{0}, object type to move {1}, object has property -position- {2}, calback is valid {3}"\
 		.format([name, item.get_class() if item else "null", "position" in item, finish_callback.is_valid()]))
