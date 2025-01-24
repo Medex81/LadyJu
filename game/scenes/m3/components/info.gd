1aa2c3fd295e300ef6861ae0e5c18af1170f2051
Sergey Nikitin <medex81@vk.com>
2025-01-24 19:10:32 +0300

    Изменения в базовом компоненте cristal. Главный компонент теперь info остальные - дочерние по отношению к нему. Устанавливаем z index для корректного рендеринга(узел в топе рендера). Для damage установлено свойство is_one_shot = true так как должны отработать один раз на матче. Имя устанавливаем сигналом в matcher от info.

--- game/scenes/m3/components/info.gd
+++ game/scenes/m3/components/info.gd
@@ -1,16 +1,16 @@
 # Компонент информации отвечает за индивидуальные параметры предмета, например название и тип.
 # Компонент может выступать контейнером для других компонент с параметрами для типов.
+# Главный компонент info остальные - дочерние по отношению к нему.
 
 extends Node
 
 class_name InfoComponent
 
-@export var item_name:String = "NONE"
-@export var cell_size:int = 128
+const none = "NONE"
+@export var item_name:String = none
 
-func get_item_name()->String:
-	return item_name
-	
-func get_item_size()->int:
-	return cell_size
+signal send_name_change(_item_name:String)
+
+func _ready() -> void:
+	send_name_change.emit(item_name)
 	
