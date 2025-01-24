748d4dd4db9d1c986f71a8674fab38d7a58445ed
Sergey Nikitin <medex81@vk.com>
2025-01-24 19:15:45 +0300

    Для контейнера damager добавим флаг использования, так как он может быть активирован не только своим компонентом дамага но и  внешним.

--- game/scenes/m3/components/damager.gd
+++ game/scenes/m3/components/damager.gd
@@ -4,12 +4,20 @@ extends Node2D
 
 class_name DamageContainerComponent
 
+# компонент уже был использован
+var is_damaged:bool = false
+
 func start():
+	if is_damaged:
+		return
+	is_damaged = true
 	for child in get_children():
+		# отвязываем компоненты дамга от себя и передаём основной сцене.
+		# смысл в том, чтобы воздействие компонента продолжалось и после уничтожения контейнера(self)
 		if child is DamageComponent:
 			remove_child(child)
-			get_tree().current_scene.add_child(child)
+			get_tree().current_scene.call_deferred("add_child", child)
 			child.global_position = global_position
-			child.visible = true
+			# активировать компонент, запускается нанесение урона.
 			child.activate = true
 		
