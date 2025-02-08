285c2b17cdb238739aa9bc81d8e9cb21951f05c6
Sergey Nikitin <medex81@vk.com>
2025-02-08 16:51:20 +0300

    В компоненте MoverComponent заменяем проверку движения по таймеру на запрос к компонентам на движение по событию. Замена контейнера с зонами бронирования перемещения - на прикрепленную к компоненту динамическую зону перемещения.

--- game/scenes/m3/components/parent_mover.gd
+++ game/scenes/m3/components/parent_mover.gd
@@ -57,7 +57,6 @@ var _cell_size = 128
 @export var _width_frame = 30
 # немного оптимизации, не дёргаем зря метод, а обращаемся к полю с указателем на родителя
 @onready var _parent = get_parent()
-@export var  _get_item_size_fn = "get_item_size"
 
 var is_moved:bool = false
 var _total_width:int
@@ -68,30 +67,31 @@ var _to_top:Vector2i
 var _to_right:Vector2i
 var _to_left:Vector2i
 var is_falling:bool = true
-
 # при переходе, запоминаем квардат куда прибудем. Это нужно для синхронизации перемещений с другими предметами
-# для избежания двойного занятия позиции. Словарь доступен из всех компонент перемещения.
-static var occupied:Dictionary
+var moving_to_rect:Rect2i = Rect2i(0, 0, 0, 0)
+var is_active:bool = false
+
 # для свапа предметов нужно помнить предмет отправитель и получатель для обмена позициями.
 static var swap_node:MoverComponent = null
+const mover_group = "movers"
 
 # оповещаем о событии остановки или начала движения предмета. Это нужно, например, для проверки матчинга.
 signal send_swap_done()
 signal send_start_step()
 signal send_stop()
 
-# когда предмет остановился, он проверяет соседей на матч или движение один раз. Если внизу предмет
-# пропал, нужно включить перемещение.
-func _on_timer_timeout():
-	call_deferred("move")
-	
 # проверяем, пустое место которое мы нашли уже кем-то занято для перемещения?
 func _is_occupied(rect:Rect2i)->bool:
-	for ocup_item in occupied:
-		if rect.intersects(occupied[ocup_item]):
+	for mover in get_tree().get_nodes_in_group(mover_group):
+		if mover != self and mover.is_moved and rect.intersects(mover.moving_to_rect):
 			return true
 	return false
-	
+
+func check_move_all():
+	for mover in get_tree().get_nodes_in_group(mover_group):
+		if not mover.is_moved and mover.is_active: 
+			mover.call_deferred("move")
+
 # предмет находится в состоянии движения/не остановился - с таким предметом нельзя матчится и т.д.
 func is_fall()->bool:
 	return is_falling
@@ -116,7 +116,6 @@ func direct()->Vector2i:
 	if cld_d:
 		# статический объект - останавливаемся, по нему не скользим
 		if cld_d is StaticBody2D:
-			$Timer.stop()
 			is_falling = false
 			send_stop.emit()
 			return Vector2i.ZERO
@@ -144,19 +143,12 @@ func direct()->Vector2i:
 		send_stop.emit()
 	is_falling = false
 	return Vector2i.ZERO
-	
-# Иногда узел удаляется ещё в момент работы твинка и его сигнал о завершении не вызывает корутину,
-# которая блокирует код завершения а методе перемещения на клетку. При выходе узла из дерева,
-# руками удаляем запись о бронировании клетки для перемещения если она не была удалена корутиной. 
-func _exit_tree() -> void:
-	occupied.erase(self)
-	swap_data.clean()
-	
+
 # двигать родительский узел на расстояние размера клетки
 # is_automove = true перезапускать перемещение после завершения движения автоматически
 func move(_direct:Vector2i = Vector2i.ZERO, is_automove:bool = true):
 	# сейчас движемся, как закончим можно начинать следующее движение, а пока отбрасываем запрос
-	if is_moved:
+	if is_moved or not is_active:
 		return
 	# запросили движение без указания направления - значит выбираем его сами
 	if _direct == Vector2i.ZERO:
@@ -170,18 +162,18 @@ func move(_direct:Vector2i = Vector2i.ZERO, is_automove:bool = true):
 	is_moved = true
 	# заносим в общий список всех компонент движения квадрат куда будем перемещаться, иначе а тот же
 	# квадрат одновременно с нами могут двигаться и другие предметы.
-	occupied[self] = Rect2i(Vector2i(_parent.global_position) + _direct, _to_down_right)
+	moving_to_rect = Rect2i(Vector2i(global_position) + _direct, _to_down_right)
 	# двигаем родителя через твин по свойству позиции
 	var move_tween = get_tree().create_tween()
-	move_tween.tween_property(_parent, "global_position", _parent.global_position + Vector2(_direct) , _move_time)
+	move_tween.tween_property(_parent, "global_position", global_position + Vector2(_direct) , _move_time)
 	# тут будет выход из метода и формирование корутины которая активируется при получении сигнала
 	# от завершения твина. После получения сигнала, корутина установит на выполнение код лежащий ниже
 	await move_tween.finished
-	occupied.erase(self)
+	moving_to_rect = Rect2i(0, 0, 0, 0)
 	is_moved = false
 	# отложенный на следующий кадр вызов себя для симуляции беспрерывного падения
 	if is_automove:
-		call_deferred("move")
+		check_move_all()
 		# если у нас автоперемещение и активный свап - сбрасываем свап
 		# обработка свапа по какой-то причине не отработала 
 		if swap_data.is_active():
@@ -190,6 +182,9 @@ func move(_direct:Vector2i = Vector2i.ZERO, is_automove:bool = true):
 	else:
 		# завершилось перемещение при свапе, уведомление с указанием состояния остановки
 		send_stop.emit()
+
+func _exit_tree() -> void:
+	check_move_all()
 
 # расчёт смещения из текущего предмета в указанный с учётом расположения по соседству
 func _get_direction_to(item:MoverComponent)->Vector2i:
@@ -225,15 +220,14 @@ func _on_input_event(_viewport, event, _shape_idx):
 				if _direct != Vector2i.ZERO:
 					swap_data.begin(swap_node, _direct)
 					swap_node.swap_data.begin(self, -_direct)
-					# делаем одно перемещение на шаг без автоперемещения далее
 					move(_direct, false)
 					swap_node.move(-_direct, false)
 				
 			swap_node = null
 
 func _ready() -> void:
-	if _parent.has_method(_get_item_size_fn):
-		_cell_size = _parent.call(_get_item_size_fn)
+	if _parent is InfoComponent:
+		_cell_size = _parent.item_size
 		_total_width = _cell_size + _width_frame
 		_to_down_left = Vector2i(-_cell_size, _cell_size)
 		_to_down_right = Vector2i(_cell_size, _cell_size)
@@ -241,7 +235,7 @@ func _ready() -> void:
 		_to_top = -_to_down
 		_to_right = Vector2i(_cell_size, 0)
 		_to_left = -_to_right
-		
+
 # компоненту нужно вернуться назад
 func revert_move():
 	# находимся в состоянии активного свапа
@@ -271,5 +265,5 @@ func revert_move():
 # Таймер заставляет узел двигаться, но узлы в генераторе не должны двигаться. Для этого мы отключаем
 # у компонента таймер и включаем его при активации - сигналом извне(обычно от родителя в игровой области сцены).
 func _on_visible_on_screen_notifier_2d_screen_entered() -> void:
-	$Timer.autostart = true
-	$Timer.start()
+	is_active = true
+	call_deferred("move")
