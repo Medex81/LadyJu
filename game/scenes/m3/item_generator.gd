62a01422c6b4b052c4fd331caac674a0b5da960c
Sergey Nikitin <medex81@vk.com>
2025-02-17 15:03:19 +0300

    Для ItemGenerator добавлена механика запроса списков предметов по типам. Добавлена механика запроса создания матчера по имени.

--- game/scenes/m3/item_generator.gd
+++ game/scenes/m3/item_generator.gd
@@ -9,7 +9,10 @@ extends Node
 class_name ItemGenerator
 
 var _generate_dynamic_items:Array[InfoComponent]
+var _item_names:Array[String]
 var _generate_matcher_items:Dictionary
+var _matcher_names:Array[String]
+enum EItemType{ITEM, MATCHER}
 
 func _ready() -> void:
 	_generate_dynamic_items.clear()
@@ -20,10 +23,13 @@ func _ready() -> void:
 			var arr = _generate_matcher_items.get(node.match_count, [])
 			arr.append(node)
 			_generate_matcher_items[node.match_count] = arr
+			_matcher_names.append(node.get_item_name())
 			continue
 		if node is InfoComponent:
 			_generate_dynamic_items.append(node)
-			
+			_item_names.append(node.get_item_name())
+	_matcher_names.make_read_only()
+	_item_names.make_read_only()
 	if _generate_dynamic_items.is_empty():
 		print("Error. Generator node has not a nodes with components for dynamics.")
 	if _generate_matcher_items.is_empty():
@@ -61,5 +67,23 @@ func generate_matcher(match_count:int, direct_h:bool = true)->MatchInfoComponent
 			var item = arr.front().duplicate()
 			item.global_position = Vector2i.ZERO
 			return item
-		
 	return null
+	
+func get_matcher(matcher_name:String)->MatchInfoComponent:
+	if not _generate_matcher_items.is_empty():
+		# предметы подходят по количеству предметов для матча
+		for arr_key in _generate_matcher_items:
+			for item in _generate_matcher_items[arr_key]:
+				if item.get_item_name() == matcher_name:
+					var new_item = item.duplicate()
+					new_item.position = Vector2i.ZERO
+					return new_item
+	return null
+
+func get_name_list(type:EItemType)->Array[String]:
+	match type:
+		EItemType.ITEM:
+			return _item_names
+		EItemType.MATCHER:
+			return _matcher_names
+	return []
