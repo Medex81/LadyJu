ef7b1a05a5859d4884eca71a49cbeecbfe1a0866
Sergey Nikitin <medex81@vk.com>
2025-03-02 18:49:08 +0300

    Финальный эффект удаляет спрайт и самоудаляется после завершения эффекта.

--- game/scenes/m3/components/view.gd
+++ game/scenes/m3/components/view.gd
@@ -2,18 +2,24 @@ extends Sprite2D
 
 class_name ViewComponent
 
-@export var _start_effect:Node2D = null
-@export var _stop_effect:Node2D = null
-@export var _end_effect:Node2D = null
-
-signal send_finish_effect()
+@export var _start_effect:Node = null
+@export var _stop_effect:Node = null
+@export var _end_effect:Node = null
 
 func run_end_effect():
+	texture = null
+	for effect in get_children():
+		if effect != _end_effect:
+			effect.visible = false
 	if _end_effect:
 		if _end_effect is GPUParticles2D:
 			_end_effect.emitting  = true
 			await _end_effect.finished
-	send_finish_effect.emit()
+		if _end_effect is EffectContainer and _end_effect.has_effects():
+			_end_effect.start()
+			await _end_effect.send_effects_done
+			
+	queue_free()
 	
 func has_end_effect()->bool:
 	return _end_effect != null
