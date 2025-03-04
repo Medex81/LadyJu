40f4bf4451aebd871bc9ffaa995c99b365a43576
Sergey Nikitin <medex81@vk.com>
2025-03-04 11:33:34 +0300

    В компоненте ExchangeDamageComponent задаём через настройки название группы кристаллов.

--- game/scenes/m3/components/damage/exchange_damage.gd
+++ game/scenes/m3/components/damage/exchange_damage.gd
@@ -3,8 +3,7 @@ extends BaseDamageComponent
 class_name ExchangeDamageComponent
 
 @export var change_percent:float = 0.5
-#@export var damage_timeout:float = 1.0
-const group_crystals = "crystals"
+@export var group_crystals = "crystals"
 
 func start(item_name = null):
 	if item_name is String and not item_name.is_empty():
@@ -14,14 +13,10 @@ func start(item_name = null):
 			if item.get_item_name() != item_name and item.is_active == true:
 				without_arr.append(item)
 		without_arr.shuffle()
-		#print("count ", without_arr.size(), " ", item_name)
 		var count = int(without_arr.size() * change_percent)
 		while count > 0:
 			count -= 1
-			var new_item = without_arr[count].change_to(item_name)
-			#if new_item != null:
-				#var detonator = DetonatorComponent.new()
-				#var parent = new_item.get_parent()
-				#parent.add_child(detonator)
-				#detonator.start(damage_timeout)
-	#super.start()
+			if without_arr[count].is_item_name_item(item_name):
+				without_arr[count].change_to_item(item_name)
+			else:
+				without_arr[count].change_to_matcher(item_name)
