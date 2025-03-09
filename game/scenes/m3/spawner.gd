8e46db0ea15c337c29f1c0ecb07e6e7d504d0ba0
Sergey Nikitin <medex81@vk.com>
2025-03-09 16:04:37 +0300

    Название группы генератора перенесено в класс генератора.

--- game/scenes/m3/spawner.gd
+++ game/scenes/m3/spawner.gd
@@ -2,8 +2,8 @@ extends Area2D
 
 class_name Spawner
 
-@export var item_generator_group_name:String = "item_generator"
-@onready var item_generator:ItemGenerator = get_tree().get_first_node_in_group(item_generator_group_name)
+#@export var item_generator_group_name:String = "item_generator"
+@onready var item_generator:ItemGenerator = get_tree().get_first_node_in_group(ItemGenerator.group_name)
 
 func _ready():
 	if not has_overlapping_bodies():
@@ -12,10 +12,9 @@ func _ready():
 func spawn_item()->bool:
 	# запрос на генерацию предмета матчера если подходит по количеству
 	if item_generator:
-		var item = item_generator.generate_item()
+		var item = item_generator.get_item()
 		if item:
 			add_child(item)
-			item.visible = true
 			return true
 	return false
 
