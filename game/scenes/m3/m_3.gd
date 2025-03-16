8198a8bb747eea5f30a1fd2c0c452e7e099aaa63
Sergey Nikitin <medex81@vk.com>
2025-03-17 01:09:31 +0300

    tech

--- game/scenes/m3/m_3.gd
+++ game/scenes/m3/m_3.gd
@@ -2,21 +2,10 @@ extends Control
 
 class_name M3
 
-enum EEvent{NO_HINT}
-
-const group_name:String = "m3_scene"
-const events_fn:String = "events"
-
 @export var _base_m3_layer:BaseM3Layer = null
 @export var _move_timeout:float = 0.2 
 var timeout_counter:float = 0
 @onready var tree = get_tree()
-
-func events(event:EEvent):
-	match event:
-		EEvent.NO_HINT:
-			if _base_m3_layer != null:
-				_base_m3_layer.layer_completed(false)
 				
 func _physics_process(delta: float) -> void:
 	timeout_counter += delta
