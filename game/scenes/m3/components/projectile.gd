281445af1092a5c62e96c5f5cd4e2f931dc69316
Sergey Nikitin <medex81@vk.com>
2025-03-23 19:22:44 +0300

    Для предметов которые движутся на цель добавлено распределение целей.

--- game/scenes/m3/components/projectile.gd
+++ game/scenes/m3/components/projectile.gd
@@ -4,6 +4,8 @@ var tween:Tween = null
 var to_item:InfoComponent = null
 var from_item:InfoComponent = null
 var start_global_pos:Vector2
+# список с разными целями по квестам для уровня
+static var processed_aims:Array[InfoComponent]
 
 func start(_self_item:InfoComponent, item_name:String = ""):
 	if _self_item != null:
@@ -16,12 +18,18 @@ func start(_self_item:InfoComponent, item_name:String = ""):
 			#var random_item_name = quest_item_names.pick_random()
 			var current_quest_item_in_field:Array[InfoComponent]
 			for info_comp in get_tree().get_nodes_in_group(InfoComponent.group_name):
-				if info_comp is InfoComponent and info_comp.is_interactive and info_comp.get_item_name() in quest_item_names:
+				if info_comp is InfoComponent and info_comp.is_interactive and info_comp.get_item_name() in quest_item_names\
+				# цель не должна быть выбрана другими предметами этого класса - удар по разным целям
+				and not info_comp in processed_aims:
 					current_quest_item_in_field.append(info_comp)
+			if current_quest_item_in_field.is_empty() and not processed_aims.is_empty():
+				current_quest_item_in_field.append(processed_aims.front())
+				
 			if not current_quest_item_in_field.is_empty():
 					var random_quest_item:InfoComponent = current_quest_item_in_field.pick_random() as InfoComponent
 					if random_quest_item != null:
 						to_item = random_quest_item
+						processed_aims.append(to_item)
 						start_global_pos = random_quest_item.global_position
 						run_tween()
 			else:
@@ -51,6 +59,8 @@ func run_tween():
 	await tween.finished
 	if to_item != null:
 		if to_item.global_position == from_item.global_position:
+			# убираем из списка целей текущую которую поразили
+			processed_aims.erase(to_item)
 			send_done.emit()
 	else:
 		call_deferred("start", from_item)
