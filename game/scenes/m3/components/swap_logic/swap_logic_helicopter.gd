e2eced12709f363e8e7a8a5b816d48e3a64940b0
Sergey Nikitin <medex81@vk.com>
2025-03-25 15:46:13 +0300

    Для логики свапа вертолетика добавлен выбор второго матчера и свой для каждого набор реакций.

--- game/scenes/m3/components/swap_logic/swap_logic_helicopter.gd
+++ game/scenes/m3/components/swap_logic/swap_logic_helicopter.gd
@@ -3,11 +3,27 @@ extends BaseSwapLogicComponent
 @export var copy_count:int = 1
 
 func start(item:InfoComponent, second_swap:InfoComponent):
-	# при особых условиях движения снимаем усиление удара с предметов
-	if second_swap != null and item.get_item_name() == second_swap.get_item_name() and item.damager_component:
-		item.damager_component.set_swap_item_name("")
 		
-	for count in range(copy_count):
-		var new_item = item.get_matcher_for_name()
-		item.get_parent().add_child(new_item)
-		new_item.finalize()
+	if second_swap is MatchInfoComponent:
+		match second_swap.get_item_name():
+			"amulet":
+				if item.damager_component:
+					item.damager_component.set_swap_item_name("")
+			"helicopter":
+				if item.damager_component:
+					item.damager_component.set_swap_item_name("")
+				if second_swap != null and second_swap.damager_component:
+					second_swap.damager_component.set_swap_item_name("")
+				for count in range(copy_count):
+					var new_item = item.get_matcher_for_name()
+					item.get_parent().add_child(new_item)
+					new_item.finalize()
+			_:
+				if item.damager_component:
+					item.damager_component.set_swap_item_name("")
+				if second_swap != null and second_swap.damager_component:
+					second_swap.damager_component.set_swap_item_name("")
+				second_swap.get_parent().remove_child(second_swap)
+				item.add_child(second_swap)
+				second_swap.position = Vector2.ZERO
+				second_swap.is_interactive = false
