ccfe9abc31e8de04f7ae6e2c5b2e2873d77c92d1
Sergey Nikitin <medex81@vk.com>
2025-03-25 15:51:17 +0300

    В компоненте PMoverComponent заменяем проверку разрешения движения на запрос из info метода is_movable. Добавляем логику проверки swap при свапе и двойном тапе. Логика теперь добавляется в info.

--- game/scenes/m3/components/pmover.gd
+++ game/scenes/m3/components/pmover.gd
@@ -42,7 +42,7 @@ func _ready() -> void:
 	moving_to_rect = Rect2(global_position, cell_size)
 	
 func try_move():
-	if is_moving == true or (info_component != null and (not info_component.is_active or info_component.is_blocked())):
+	if is_moving == true or (info_component != null and not info_component.is_movable()):
 		return
 
 	# проверяем находимся ли мы на чём-то с чего нельзя соскользнуть
@@ -91,14 +91,18 @@ func swap_move(direct:Vector2, second_mover:PMoverComponent = null, is_step_coun
 	is_moving = true
 	var second_name = second_mover.info_component.get_item_name() if second_mover != null and second_mover.info_component != null else ""
 	if info_component and direct != Vector2.ZERO:
+		# в компонент нанесения урона устанавливаем имя второго предмета для определения нужно ли усиление
 		if _damager_component and not second_name.is_empty() and info_component is MatchInfoComponent:
 			_damager_component.set_swap_item_name(second_name)
+		# бронируем позицию для перехода
 		moving_to_rect = Rect2(global_position + direct * _cell_width, cell_size)
+		
 		var move_tween = get_tree().create_tween()
 		move_tween.tween_property(info_component, "global_position", global_position + direct * _cell_width, _move_time)
 		await move_tween.finished
 		
 		if info_component is MatchInfoComponent:
+			# всё - компонент больше нельзя использовать
 			move_state = EMoveState.FINAL
 			info_component.proc_swap_logic(second_mover.info_component)
 			if _swap_move_logic != null:
@@ -106,7 +110,7 @@ func swap_move(direct:Vector2, second_mover:PMoverComponent = null, is_step_coun
 				_swap_move_logic.start(info_component)
 				await _swap_move_logic.send_done
 				is_moving = false
-				info_component.finalize()
+			info_component.finalize()
 		else:
 			matching()
 		if is_step_counting:
@@ -144,6 +148,7 @@ func _on_input_event(_viewport, event, _shape_idx):
 		if event.is_released() and QuestsPanel.is_no_step == false:
 			# тап на матчер, подрывам его одного
 			if swap_node == self and info_component is MatchInfoComponent:
+				info_component.proc_swap_logic()
 				if not info_component.is_blocked() and _swap_move_logic != null:
 					move_state = EMoveState.FINAL
 					is_moving = true
@@ -158,11 +163,11 @@ func _on_input_event(_viewport, event, _shape_idx):
 					swap_node = null
 					return
 					
+				# сматчивание с матчером
 				if info_component is MatchInfoComponent or swap_node.info_component is MatchInfoComponent:
 					var direct = global_position.direction_to(swap_node.global_position).sign()
 					swap_move(direct, swap_node, false)
 					swap_node.swap_move(-direct, self)
-
 				elif set_fake_item_name(swap_node.get_item_name()) and swap_node.set_fake_item_name(get_item_name()) \
 				and (swap_node.check_match() or check_match()):
 					set_fake_item_name()
