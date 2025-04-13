e1c37ad7ee4a6f89e074aca679257fbe3bb49684
Sergey Nikitin <medex81@vk.com>
2025-04-13 23:42:26 +0300

    В бары статы добавлена единная логика работы с PS через выбор имени поля.

--- game/scenes/ui/hud/bar.gd
+++ game/scenes/ui/hud/bar.gd
@@ -1,13 +1,19 @@
 extends Panel
 
-@onready var player_state:PlayerState = Globals.player_state
-
-func _ready() -> void:
-	player_state.send_coins_change.connect(set_value)
+@export var save_value_type:PlayerState.EPS = PlayerState.EPS.NONE
+var save_value:PlayerStateValueInt = null
 
 func set_value(value:int):
 	$value.text = str(value)
+
+func _ready() -> void:
+	if save_value_type != PlayerState.EPS.NONE:
+		save_value = Globals.player_state.get_int_value(save_value_type)
+		if save_value:
+			save_value.send_value_change.connect(set_value)
+			set_value(save_value.value)
 	
 func _exit_tree() -> void:
-	if player_state.send_coins_change.is_connected(set_value):
-		player_state.send_coins_change.disconnect(set_value)
+	if save_value:
+		if save_value.send_value_change.is_connected(set_value):
+			save_value.send_value_change.disconnect(set_value)
