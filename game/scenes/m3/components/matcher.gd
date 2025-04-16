0af222e1b791810d651168a5154fe61ddaf49869
Sergey Nikitin <medex81@vk.com>
2025-04-16 15:10:03 +0300

    Для модуля матчера компонента MatcherComponent  добавлены комментарии в код.

--- game/scenes/m3/components/matcher.gd
+++ game/scenes/m3/components/matcher.gd
@@ -10,14 +10,18 @@ var _cell_size:int = 128
 var _cell_diagonal:int = 181
 # радиус поиска - 2 размера клетки
 var _cell_2size:int = 256
+# расстояния между клетками
 enum EDistance{NONE, CELL, DIAGONAL, CELL_2}
+# тип сматчивания
 enum EMatcher{NONE, LINE3, LINE4_H, LINE4_V, LINE5, ANGLE5, SQUARE4, T4, T5, T6, T7}
 @export var cell_offset:int = 5
 @export var info_component:InfoComponent = null
+# подписка на сохраняемое в ПС значение
 @export var save_value_type:PlayerState.EPS = PlayerState.EPS.NONE
 var save_coins:PlayerStateValueInt = null
 
 func _ready() -> void:
+	# подписываемся на изменение указанного значения в ПС
 	if save_value_type != PlayerState.EPS.NONE:
 		save_coins = Globals.player_state.get_int_value(save_value_type)
 	if info_component:
@@ -26,7 +30,7 @@ func _ready() -> void:
 		_cell_diagonal = int(_cell_size * _sqrt_2)
 		_cell_2size = _cell_size * 2
 		
-# проверяем дистанцию по компонента, ответ перечисляемым типом
+# проверяем дистанцию до компонента, ответ перечисляемым типом
 func distance_to_enum(area:MatcherComponent)->EDistance:
 	var distance = global_position.distance_to(area.global_position)
 	if absi(distance - _cell_size) < cell_offset:
@@ -37,6 +41,7 @@ func distance_to_enum(area:MatcherComponent)->EDistance:
 		return EDistance.CELL_2
 	return EDistance.NONE
 	
+# найти соседей с тем же именем
 func get_neighbors(_compose_hints:Dictionary):
 	_compose_hints.clear()
 	# получаем два списка с компонентами нашего типа и другие в области обнаружения
@@ -55,12 +60,17 @@ func get_neighbors(_compose_hints:Dictionary):
 
 func check_detector_collisions(with_remove:bool = true)->EMatcher:
 	var _compose_hints:Dictionary
+	# собираем соседей этой клетки
 	get_neighbors(_compose_hints)
 
+	# есть соседи с тем же именем по горизонтали или вертикали 
 	if _compose_hints.has(EDistance.CELL):
 		var cell_link_count = 0
 		var matched_cells:Array[MatcherComponent] 
 		var neighbors_cell_count = _compose_hints[EDistance.CELL].size()
+		# есть соседи с тем же именем через одну клетку
+		# находим - линки (соединения) ближнего и дальнего(через одну клетку) соседа
+		# по линкам можно идентифицировать тип матча без привязки к пространственной позиции
 		if _compose_hints.has(EDistance.CELL_2):
 			for cell in _compose_hints[EDistance.CELL]:
 				for _cell in _compose_hints[EDistance.CELL_2]:
@@ -69,6 +79,7 @@ func check_detector_collisions(with_remove:bool = true)->EMatcher:
 						matched_cells.append(_cell)
 		matched_cells.append_array(_compose_hints[EDistance.CELL])
 		var match_result:EMatcher = EMatcher.NONE
+		# матч в форме - Т
 		if neighbors_cell_count == 3:
 			match  cell_link_count:
 				0:
@@ -79,7 +90,7 @@ func check_detector_collisions(with_remove:bool = true)->EMatcher:
 					match_result = EMatcher.T6
 				3:
 					match_result = EMatcher.T7
-					
+		# линейный, квадратный и угловой типы матчей
 		if neighbors_cell_count == 2:
 			match  cell_link_count:
 				0, 1:
@@ -103,14 +114,15 @@ func check_detector_collisions(with_remove:bool = true)->EMatcher:
 						match_result = EMatcher.LINE5
 					else:
 						match_result = EMatcher.ANGLE5
-						
+		# матч линейный 3 и Т(4)
 		if neighbors_cell_count == 1:
 			match cell_link_count:
 				1: match_result = EMatcher.LINE3
 				2: match_result = EMatcher.T4
-			
+		# есть результат для сматчивания
 		if match_result != EMatcher.NONE:
 			if with_remove:
+				# удалить сматченные простые предметы и заменить на матчер
 				remove_and_change(matched_cells, match_result)
 			return match_result
 			
@@ -119,8 +131,10 @@ func check_detector_collisions(with_remove:bool = true)->EMatcher:
 func remove_and_change(neighbors:Array[MatcherComponent], change_enum:EMatcher):
 	if info_component != null:
 		if change_enum > EMatcher.LINE3:
+			# награда за сматчивание
 			if save_coins:
 				save_coins.value += int(change_enum) * 10
+			# заменяем простой предмет на матчер в этой клетке
 			info_component.change_to_matcher_enum(change_enum)
 		else:
 			if save_coins:
