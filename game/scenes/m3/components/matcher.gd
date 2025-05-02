9dcfc5e5d263c8b507a6689229c77e17bbac80b0
Sergey Nikitin <medex81@vk.com>
2025-05-02 17:07:30 +0300

    Для компонента MatcherComponent упрощён поиск матчинга соседей. сокращено количество позиций матчеров. Добавлена проверка предмета на финализацию перед матчингом для избежания захвата уже удаленных , но ещё существующих предметов. В конфигурации удалена ссылка на подсказку, она переносится в игровое поле м3.

--- game/scenes/m3/components/matcher.gd
+++ game/scenes/m3/components/matcher.gd
@@ -13,7 +13,7 @@ var _cell_2size:int = 256
 # расстояния между клетками
 enum EDistance{NONE, CELL, DIAGONAL, CELL_2}
 # тип сматчивания
-enum EMatcher{NONE, LINE3, LINE4_H, LINE4_V, LINE5, ANGLE5, SQUARE4, T4, T5, T6, T7}
+enum EMatcher{NONE, LINE4_H, LINE4_V, COUNT5, COUNT6, COUNT7}
 @export var cell_offset:int = 5
 @export var info_component:InfoComponent = null
 # подписка на сохраняемое в ПС значение
@@ -40,13 +40,13 @@ func distance_to_enum(area:MatcherComponent)->EDistance:
 	if absi(distance - _cell_2size) < cell_offset:
 		return EDistance.CELL_2
 	return EDistance.NONE
-	
+
 # найти соседей с тем же именем
 func get_neighbors(_compose_hints:Dictionary):
 	_compose_hints.clear()
 	# получаем два списка с компонентами нашего типа и другие в области обнаружения
 	for area in $detector.get_overlapping_areas():
-		if area is MatcherComponent:
+		if area is MatcherComponent and area.info_component and not area.info_component.is_died:
 			# дистанция в размерах клетки
 			var distance = distance_to_enum(area)
 			# свой предмет исключаем из поисковой выдачи
@@ -58,98 +58,67 @@ func get_neighbors(_compose_hints:Dictionary):
 					arr.append(area)
 					_compose_hints[distance] = arr
 
-func check_detector_collisions(with_remove:bool = true)->EMatcher:
+func match_detector(with_remove:bool = true)->bool:
 	var _compose_hints:Dictionary
+	var matched_cells1:Array[MatcherComponent]
+	var matched_cells2:Array[MatcherComponent]
 	# собираем соседей этой клетки
 	get_neighbors(_compose_hints)
-
-	# есть соседи с тем же именем по горизонтали или вертикали 
+	# добавляем линки между клетками
+	if _compose_hints.has(EDistance.CELL_2) and _compose_hints.has(EDistance.CELL):
+		for cell1 in _compose_hints[EDistance.CELL]:
+			for cell2 in _compose_hints[EDistance.CELL_2]:
+				if cell1.distance_to_enum(cell2) == EDistance.CELL:
+					matched_cells1.append(cell1)
+					matched_cells2.append(cell2)
+					continue
+	# добавляем одиночные клетки линкующиеся через один (через основную)
 	if _compose_hints.has(EDistance.CELL):
-		var cell_link_count = 0
-		var matched_cells:Array[MatcherComponent] 
-		var neighbors_cell_count = _compose_hints[EDistance.CELL].size()
-		# есть соседи с тем же именем через одну клетку
-		# находим - линки (соединения) ближнего и дальнего(через одну клетку) соседа
-		# по линкам можно идентифицировать тип матча без привязки к пространственной позиции
-		if _compose_hints.has(EDistance.CELL_2):
-			for cell in _compose_hints[EDistance.CELL]:
-				for _cell in _compose_hints[EDistance.CELL_2]:
-					if cell.distance_to_enum(_cell) == EDistance.CELL:
-						cell_link_count += 1
-						matched_cells.append(_cell)
-		matched_cells.append_array(_compose_hints[EDistance.CELL])
-		var match_result:EMatcher = EMatcher.NONE
-		# матч в форме - Т
-		if neighbors_cell_count == 3:
-			match  cell_link_count:
-				0:
-					match_result = EMatcher.T4
-				1:
-					match_result = EMatcher.T5
-				2:
-					match_result = EMatcher.T6
-				3:
-					match_result = EMatcher.T7
-		# линейный, квадратный и угловой типы матчей
-		if neighbors_cell_count == 2:
-			match  cell_link_count:
-				0, 1:
-					if _compose_hints[EDistance.CELL].front().distance_to_enum(_compose_hints[EDistance.CELL].back()) == EDistance.DIAGONAL:
-						if _compose_hints.has(EDistance.DIAGONAL):
-							for cell in _compose_hints[EDistance.DIAGONAL]:
-								if cell.distance_to_enum(_compose_hints[EDistance.CELL].front()) == EDistance.CELL and \
-								cell.distance_to_enum(_compose_hints[EDistance.CELL].back()) == EDistance.CELL:
-									matched_cells.append(cell)
-									match_result = EMatcher.SQUARE4
-					elif _compose_hints[EDistance.CELL].front().distance_to_enum(_compose_hints[EDistance.CELL].back()) == EDistance.CELL_2 \
-					and cell_link_count == 1:
-						if absi(_compose_hints[EDistance.CELL].front().global_position.x - _compose_hints[EDistance.CELL].back().global_position.x) < cell_offset:
-							match_result = EMatcher.LINE4_V
-						else:
-							match_result = EMatcher.LINE4_H
-					elif _compose_hints[EDistance.CELL].front().distance_to_enum(_compose_hints[EDistance.CELL].back()) == EDistance.CELL_2:
-						match_result = EMatcher.LINE3
-				2:
-					if _compose_hints[EDistance.CELL].front().distance_to_enum(_compose_hints[EDistance.CELL].back()) == EDistance.CELL_2:
-						match_result = EMatcher.LINE5
-					else:
-						match_result = EMatcher.ANGLE5
-		# матч линейный 3 и Т(4)
-		if neighbors_cell_count == 1:
-			match cell_link_count:
-				1: match_result = EMatcher.LINE3
-				2: match_result = EMatcher.T4
-		# есть результат для сматчивания
-		if match_result != EMatcher.NONE:
-			if with_remove:
-				# удалить сматченные простые предметы и заменить на матчер
-				remove_and_change(matched_cells, match_result)
-			return match_result
-			
-	return EMatcher.NONE
+		for cell1 in _compose_hints[EDistance.CELL]:
+			if not matched_cells1.has(cell1):
+				for cell11 in _compose_hints[EDistance.CELL]:
+					if cell1 != cell11 and cell1.distance_to_enum(cell11) == EDistance.CELL_2:
+						matched_cells1.append(cell1)
+
+	matched_cells1.append_array(matched_cells2)
+	var total_size = matched_cells1.size() + 1
 	
-func remove_and_change(neighbors:Array[MatcherComponent], change_enum:EMatcher):
-	if info_component != null:
-		if change_enum > EMatcher.LINE3:
-			# награда за сматчивание
-			if save_coins:
-				save_coins.value += int(change_enum) * 10
-			# заменяем простой предмет на матчер в этой клетке
-			info_component.change_to_matcher_enum(change_enum)
-		else:
-			if save_coins:
-				save_coins.value += 10
-			info_component.finalize()
-			
-	for item in neighbors:
-		if item.info_component:
-			item.info_component.finalize()
-			
+	if info_component != null and total_size >= 3:
+		if with_remove == true:
+			if total_size == 3:
+				if save_coins:
+					save_coins.value += 1
+				matched_cells1.append(self)
+			else:
+				var matcher_item:EMatcher = EMatcher.NONE
+				match total_size:
+					4:
+						if absi(matched_cells1.front().global_position.x - matched_cells1.back().global_position.x) < cell_offset:
+							matcher_item = EMatcher.LINE4_V
+						else:
+							matcher_item = EMatcher.LINE4_H
+					5:
+						matcher_item = EMatcher.COUNT5
+					6:
+						matcher_item = EMatcher.COUNT6
+					7:
+						matcher_item = EMatcher.COUNT7
+				# награда за сматчивание
+				if save_coins:
+					save_coins.value += total_size
+				# заменяем простой предмет на матчер в этой клетке
+				info_component.change_to_matcher_enum(matcher_item)
+			for item in matched_cells1:
+				if item.info_component:
+					item.info_component.call_deferred("finalize")
+		return true
+	return false
+
 func has_match()->bool:
-	return check_detector_collisions(false)
+	return match_detector(false)
 	
 func matching()->bool:
-	return check_detector_collisions()
+	return match_detector()
 
 func get_item_name()->String:
 	return item_name if fake_item_name.is_empty() or fake_item_name == item_name else fake_item_name
