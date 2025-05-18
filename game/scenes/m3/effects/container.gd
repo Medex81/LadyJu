b09d905a6791c310fc1de3b0477c31c577c493b6
Sergey Nikitin <medex81@vk.com>
2025-05-18 14:17:43 +0300

    В компоненте EffectContainer исправлена ошибка выбора последнего эффекта.

--- game/scenes/m3/effects/container.gd
+++ game/scenes/m3/effects/container.gd
@@ -2,21 +2,14 @@ extends Node2D
 
 class_name EffectContainer
 
-signal send_effects_done()
+var last_effect:GPUParticles2D = null
 
-func start():
-	var last_effect:GPUParticles2D = null
+func _ready() -> void:
 	for effect in get_children():
 		if effect is GPUParticles2D:
 			last_effect = effect
-			effect.emitting  = true
-	if last_effect:
-		await last_effect.finished
-		
-	send_effects_done.emit()
-	
-func has_effects()->bool:
+
+func start():
 	for effect in get_children():
 		if effect is GPUParticles2D:
-			return true
-	return false
+			effect.emitting  = true
